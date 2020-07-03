8bb785b1a7898428903dec8ed0dad420a80891ed
Ryan Campbell <Rpcampbell434@aol.com>
2020-07-02 21:05:49 -0400

    Adding card pack buttons to shop page

--- src/Components/ShopDisplay.js
+++ src/Components/ShopDisplay.js
@@ -1,15 +1,59 @@
 import React from "react";
 import { observer } from "mobx-react";
-import { PlayingCardBack } from "./PlayingCardBack";
+import { Button } from "@material-ui/core";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import { amber, grey } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: amber[800],
+    },
+    secondary: {
+      main: grey[400],
+    },
+    status: {
+      danger: amber[500],
+    },
+  },
+});
 
 export const ShopDisplay = observer(() => {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <div className="shopOptions">
-        <PlayingCardBack />
-        <PlayingCardBack />
-        <PlayingCardBack />
+        <div className="shopButtonDivs">
+          <Button
+            className="shopButtons"
+            color="primary"
+            variant="contained"
+            size="large"
+          >
+            Buy Bronze Pack
+          </Button>
+        </div>
+        <div className="shopButtonDivs">
+          <Button
+            className="shopButtons"
+            color="secondary"
+            variant="contained"
+            size="large"
+          >
+            Buy Silver Pack
+          </Button>
+        </div>
+        <div className="shopButtonDivs">
+          <Button
+            className="shopButtons"
+            color="status"
+            variant="contained"
+            size="large"
+          >
+            Buy Gold Pack
+          </Button>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 });
